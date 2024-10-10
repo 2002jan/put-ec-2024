f620aced04c2c207ffeed489f805ed4ebfdb9f1c
Michał Kamiński <mkami2002@gmail.com>
2024-10-10 14:16:52 +0200

    Fix nearest_neighbor_end.rs cost calculation

--- tsp-algos/src/greedy_heuristics/nearest_neighbor_end.rs
+++ tsp-algos/src/greedy_heuristics/nearest_neighbor_end.rs
@@ -4,20 +4,17 @@ use crate::TspAlgorithm;
 pub struct NearestNeighborEndAlgorithm {}
 
 impl TspAlgorithm for NearestNeighborEndAlgorithm {
-    fn run(cost_matrix: &CostMatrix, _points_cost: &Vec<i32>, start_from: Option<i32>) -> Vec<i32> {
+    fn run(cost_matrix: &CostMatrix, points_cost: &Vec<i32>, start_from: Option<i32>) -> Vec<i32> {
         let size = cost_matrix.size() as i32;
         let solution_size = ((size as f32) / 2.).ceil() as i32;
 
-        // Determine the starting node
         let start_node = match start_from {
             Some(start) => start,
-            None => 0 // Default to the first node if not specified
+            None => 0
         };
 
 
-        // Initialize the solution with the start node
         let mut solution = vec![start_node];
-        // Track visited nodes
         let mut visited = vec![false; cost_matrix.size()];
         visited[start_node as usize] = true;
 
@@ -32,7 +29,7 @@ impl TspAlgorithm for NearestNeighborEndAlgorithm {
 
             for next_node in 0..cost_matrix.size() as i32 {
                 if !visited[next_node as usize] {
-                    let distance = cost_matrix.get(last_node as usize, next_node as usize);
+                    let distance = cost_matrix.get(last_node as usize, next_node as usize) + points_cost[next_node as usize];
 
                     if distance < nearest_cost {
                         nearest_cost = distance;
